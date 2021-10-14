e2a4d9f51a61c2627b8422633e79797393a61522
Nuria Ciscar Garzón <nuriaciscar@hotmail.com>
2021-10-14 20:57:58 +0200

    Created the structure of the component

--- public/js/SeriesService.test.js
+++ public/js/SeriesService.test.js
@@ -12,32 +12,19 @@ describe("Given a SeriesService class", () => {
     });
   });
 
-  async deleteSerie(id) {
-    const response = await fetch(`${this.urlAPI}${id}`, {
-      method: "DELETE",
-    });
-    if (response.ok) {
-      return true;
-    }
-    throw new Error("It's not possible to delete the selected serie");
-  }
-
-  
   describe("When we call an API to delete the given id", () => {
     test("Then we should substract the id from the object", () => {
-      const object = {
-      "id": 1,
-      "name": "The Sopranos",
-      "creator": "David Chase",
-      };
+      const expected = 4;
 
       const series = new SeriesService();
-      const result = series.deleteSerie(id);
+      const newSeries = series.deleteSerie(1);
 
-      return expect(typeof result).toEqual(typeof expected);
+      const result = newSeries.length;
+
+      return expect(result).toEqual(expected);
     });
   });
 });
 
-crear obj, metode deleteserie amb parametre id,
-a.series.length 
+// crear obj, metode deleteserie amb parametre id,
+// a.series.length
